aae33b419857598576b021ffd54da96007cbeee8
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-10 14:01:29 +0200

    function called filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,14 +3,18 @@
 filtered logger
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     obfuscating info in log message
     """
-    pattern = re.compile(
-        rf'(\b{"|".join(fields)}=)[^{separator}]*?(?={separator})'
-    )
-    obf_message = pattern.sub(rf'\1{redaction}', message)
-    return obf_message
+    for field in fields:
+        pattern = re.compile(
+            rf'(\b{field}=)[^{separator}]*({separator})'
+        )
+        message = pattern.sub(rf'\1{redaction}\2', message)
+
+    return message
