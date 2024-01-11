f6fcd5b61f9fd63197f54934a17f53204b9f7910
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-11 11:02:43 +0200

    function called filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,25 @@ filtered logger
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -12,9 +31,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     obfuscating info in log message
     """
     for field in fields:
-        pattern = re.compile(
-            rf'(\b{field}=)[^{separator}]*({separator})'
-        )
-        message = pattern.sub(rf'\1{redaction}\2', message)
-
+        message = re.sub(rf'({field}=)[^{separator}]*{separator}',
+                         rf'\1{redaction}{separator}', message)
     return message
