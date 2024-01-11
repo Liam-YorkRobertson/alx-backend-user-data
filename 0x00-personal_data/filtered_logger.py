a836435f84067bc9e90dcfa2f31e963ee0261388
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-11 14:22:20 +0200

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     connects to database and retrieves data
     """
