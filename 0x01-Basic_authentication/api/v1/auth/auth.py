c8dfa0c904cb370b959a9cc83ad381e994b4e4fa
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-16 23:10:19 +0200

    api authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,9 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """
+    authorisation class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         check if path requires auth
