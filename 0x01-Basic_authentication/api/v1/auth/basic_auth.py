74836df99d9228367862501d25499ef7d61cd8e2
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-17 00:06:44 +0200

    method to decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 manages basic authorisation
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
             return None
         base64_part = auth_header.split('Basic ')[1].strip()
         return base64_part
+
+    def decode_base64_authorization_header(self,
+                                           base64_auth_header: str) -> str:
+        """
+        decode base64 auth header
+        """
+        if base64_auth_header is None or not isinstance(base64_auth_header,
+                                                        str):
+            return None
+        try:
+            decode_bytes = base64.b64decode(base64_auth_header)
+            decode_str = decode_bytes.decode('utf-8')
+            return decode_str
+        except (UnicodeDecodeError):
+            return None
