62d1653b5e3af4bb73106a3dc13cc84e910967df
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-18 23:17:45 +0200

    created session auth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -18,6 +19,8 @@ auth = None
 auth_method = getenv("AUTH_TYPE", "").lower()
 if auth_method == "basic_auth":
     auth = BasicAuth()
+elif auth_method == "session_auth":
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+manages session auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session auth class
+    """
+    pass
