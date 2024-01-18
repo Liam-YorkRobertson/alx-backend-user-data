52cfa0d975e5df66c80ce9b60bcb850cfeec19a1
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-19 01:23:27 +0200

    logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -23,9 +23,7 @@ def login():
     user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    
     session_id = auth.create_session(user.id)
-    print("Session ID:", session_id)
     user_data = user.to_json()
     response = jsonify(user_data)
     response.set_cookie(auth.session_name, session_id)
