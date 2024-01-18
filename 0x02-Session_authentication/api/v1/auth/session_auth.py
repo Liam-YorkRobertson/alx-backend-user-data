9c37ae6debb5ef076aac08ea739a602ee897670f
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-18 23:43:43 +0200

    created method that returns a user id based on a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,7 +22,6 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-    
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         returns user id based on session id
