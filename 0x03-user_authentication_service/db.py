69ba02f97dcf7c53a624a98324ccc300995a2ee4
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-25 00:58:39 +0200

    creating user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
         """
         adds a new user
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
