4a7c969574810d0134fc8da3375129c5d78e6041
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-26 00:47:27 +0200

    login function for user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -30,6 +30,22 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError as error:
         return jsonify({"message": str(error)}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    Handles user login.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
