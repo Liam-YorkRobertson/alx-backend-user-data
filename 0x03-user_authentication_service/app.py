33d9fcf60ae6ba2410be12ed5dc559da7dc01401
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-26 00:21:42 +0200

    registers a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +16,20 @@ def index():
     returns message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    register new user
+    """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError as error:
+        return jsonify({"message": str(error)}), 400
 
 
 if __name__ == "__main__":
